dc0968957a6f1861a497c18aa22c2a8857aec615
hiroakissh <hiroaki.saito1014@gmail.com>
2023-11-27 22:48:25 +0900

    add project file

---
+++ Hireica/HireicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HireicaApp.swift
+//  Hireica
+//
+//  Created by HiroakiSaito on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct HireicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
