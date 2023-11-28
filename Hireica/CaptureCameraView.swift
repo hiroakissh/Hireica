50ad3cfd602b9be3948f062ceaed308288fa8e68
hiroakissh <hiroaki.saito1014@gmail.com>
2023-11-28 23:00:34 +0900

    [add] cameraを起動するViewの作成

--- Hireica/CaptureCameraView.swift
+++ Hireica/CaptureCameraView.swift
@@ -11,18 +11,55 @@ protocol CaptureCameraViewDelegate {
     func passImage(image: UIImage)
 }
 
-struct ContentView: View {
+struct CaptureCameraView: View {
+    @Environment(\.isPresented) var presentation
+    @Environment(\.dismiss) var dismiss
+    @ObservedObject private(set) var viewModel = CaptureCameraViewModel()
+
+    var delegate: CaptureCameraViewDelegate?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            CapturePreView(captureCameraViewModel: viewModel)
+            VStack {
+                Spacer()
+                Button {
+                    viewModel.capture()
+                } label: {
+                    Image(systemName: "circle.inset.filled")
+                        .resizable(resizingMode: .stretch)
+                        .foregroundColor(.white)
+                }
+                .frame(width: 60, height: 60)
+            }
         }
-        .padding()
+        .onChange(of: viewModel.imageData, { _, imageData in
+            guard let imageData = imageData else { return }
+            delegate?.passImage(image: imageData)
+            dismiss()
+        })
+    }
+}
+
+struct CapturePreView: UIViewRepresentable {
+    
+    private var captureCameraViewModel: CaptureCameraViewModel
+
+    init(captureCameraViewModel: CaptureCameraViewModel) {
+        self.captureCameraViewModel = captureCameraViewModel
+    }
+
+    public func makeUIView(context: Context) -> some UIView {
+        let cameraView = captureCameraViewModel.previewView
+        cameraView.frame = UIScreen.main.bounds
+        captureCameraViewModel.launchCamera()
+        return cameraView
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
     }
 }
 
 #Preview {
-    ContentView()
+    CaptureCameraView()
 }
