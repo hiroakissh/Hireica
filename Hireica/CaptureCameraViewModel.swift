88f310baf2feccc26370fed43fc76f1f3cdaa698
hiroakissh <hiroaki.saito1014@gmail.com>
2023-11-28 23:00:13 +0900

    [add] SampleのViewModelの作成

--- Hireica/CaptureCameraViewModel.swift
+++ Hireica/CaptureCameraViewModel.swift
@@ -6,3 +6,82 @@
 //
 
 import Foundation
+import AVFoundation
+import UIKit
+
+class CaptureCameraViewModel: NSObject, ObservableObject {
+    @Published private(set) var imageData: UIImage?
+    var previewView: UIView = UIView()
+
+    private var session: AVCaptureSession?
+    private var photoOutput: AVCapturePhotoOutput?
+    private var captureSetting: AVCapturePhotoSettings?
+
+    func launchCamera() {
+        if let session = self.session {
+            Task {
+                session.startRunning()
+            }
+            return
+        }
+
+        guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back), 
+                let deviceInput = try? AVCaptureDeviceInput(device: device) else {
+            return
+        }
+
+        let photoOutput = AVCapturePhotoOutput()
+        let session = AVCaptureSession()
+
+        switch UIDevice.current.userInterfaceIdiom {
+        case .phone:
+            session.sessionPreset = .high
+        default:
+            session.sessionPreset = .photo
+        }
+
+        session.addInput(deviceInput)
+        session.addOutput(photoOutput)
+
+        let videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.connection?.videoRotationAngle = .greatestFiniteMagnitude
+
+        Task {
+            session.startRunning()
+        }
+
+        self.photoOutput = photoOutput
+        self.session = session
+
+        previewView.layer.insertSublayer(videoPreviewLayer, at: 0)
+    }
+
+    func closeCamera() {
+        session?.stopRunning()
+    }
+
+    func capture() {
+        Task { @MainActor in
+            let captureSetting = AVCapturePhotoSettings()
+            captureSetting.flashMode = .off
+            photoOutput?.capturePhoto(with: captureSetting, delegate: self)
+            self.captureSetting = captureSetting
+        }
+    }
+}
+
+extension CaptureCameraViewModel: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+
+        guard let fileData = photo.fileDataRepresentation(),
+              let image = UIImage(data: fileData) else {
+            return
+        }
+        imageData = image
+    }
+}
