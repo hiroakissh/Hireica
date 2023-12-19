7d43364049fe64b03b7e207af29e020d87fbaded
hiroakissh <hiroaki.saito1014@gmail.com>
2023-12-19 21:35:13 +0900

    [add] RealTimeで波形を出力するためのViewを作成

---
+++ Hireica/RealTimeRecoderView.swift
@@ -0,0 +1,18 @@
+//
+//  RealTimeRecoderView.swift
+//  Hireica
+//
+//  Created by HiroakiSaito on 2023/12/19.
+//
+
+import SwiftUI
+
+struct RealTimeRecoderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RealTimeRecoderView()
+}
