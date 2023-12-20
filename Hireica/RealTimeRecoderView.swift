68556123bad3dde537ec34f5ea907fbda6ce66b4
hiroakissh <hiroaki.saito1014@gmail.com>
2023-12-20 21:56:06 +0900

    [add] Metalを使用した書き方を残してみる

--- Hireica/RealTimeRecoderView.swift
+++ Hireica/RealTimeRecoderView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import Combine
 import AVFoundation
 import Charts
+import MetalKit
 
 class TestAudioRecorder: NSObject, ObservableObject  {
     var testAudioRecorder: AVAudioRecorder!
@@ -121,17 +122,24 @@ struct PlotView: View {
 //            .stroke(Color.blue, lineWidth: 2)
 //        }
 
-        Chart {
-            // データ構造からx, y値を取得して散布図プロット
-
-            ForEach($data) { shape in
-                // 折れ線グラフをプロット
-
-                LineMark(
-                    x: .value("x", shape.xValue),
-                    y: .value("y", shape.yValue)
-                )
-            }
+//        Chart {
+//            // データ構造からx, y値を取得して散布図プロット
+//
+//            ForEach($data) { shape in
+//                // 折れ線グラフをプロット
+//
+//                LineMark(
+//                    x: .value("x", shape.xValue),
+//                    y: .value("y", shape.yValue)
+//                )
+//            }
+//        }
+        @StateObject var recorder = TestAudioRecorder()
+        // Metal使った書き方
+        VStack {
+            AudioRecorderView(testRecorder: recorder)
+            MetalPlotView(data: recorder.audioData)
+                .frame(height: 200)
         }
     }
 }
@@ -146,3 +154,109 @@ struct PlotView: View {
 //        }
 //    }
 //}
+
+
+// Metal使った書き方
+struct MetalPlotView: UIViewRepresentable {
+    var data: [Float]
+
+    func makeUIView(context: Context) -> MTKView {
+        let metalView = MTKView()
+        metalView.device = MTLCreateSystemDefaultDevice()
+        metalView.isPaused = true
+        metalView.enableSetNeedsDisplay = true
+        metalView.contentMode = .scaleAspectFit
+        metalView.preferredFramesPerSecond = 60
+        metalView.delegate = context.coordinator
+
+        return metalView
+    }
+
+    func updateUIView(_ uiView: MTKView, context: Context) {
+        context.coordinator.data = data
+        uiView.draw()
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MTKViewDelegate {
+        var data: [Float] = []
+        var plotView: MetalPlotView
+
+        init(_ plotView: MetalPlotView) {
+            self.plotView = plotView
+        }
+
+        func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+            // Do nothing
+        }
+
+        func draw(in view: MTKView) {
+            if let commandBuffer = MetalManager.shared.commandQueue.makeCommandBuffer(),
+               let drawable = view.currentDrawable,
+               let renderPassDescriptor = view.currentRenderPassDescriptor {
+                let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+                commandEncoder?.setRenderPipelineState(MetalManager.shared.pipelineState)
+
+                // Draw the waveform using data
+                var vertices: [Float] = []
+                for (index, value) in data.enumerated() {
+                    let x = -1.0 + Float(index) / Float(data.count - 1) * 2.0
+                    let y = value * 2.0
+                    vertices.append(contentsOf: [x, y, 0.0])
+                }
+
+                commandEncoder?.setVertexBytes(vertices, length: vertices.count * MemoryLayout<Float>.size, index: 0)
+                commandEncoder?.drawPrimitives(type: .line, vertexStart: 0, vertexCount: data.count)
+
+                commandEncoder?.endEncoding()
+                commandBuffer.present(drawable)
+                commandBuffer.commit()
+            }
+        }
+    }
+}
+
+
+class MetalManager {
+    static let shared = MetalManager()
+
+    let device: MTLDevice
+    let commandQueue: MTLCommandQueue
+    let pipelineState: MTLRenderPipelineState
+
+    private init() {
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            fatalError("Metal is not supported on this device")
+        }
+        self.device = device
+        self.commandQueue = device.makeCommandQueue()!
+
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.vertexFunction = MetalManager.loadShader("vertex_main")
+        pipelineDescriptor.fragmentFunction = MetalManager.loadShader("fragment_main")
+        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+
+        do {
+            self.pipelineState = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        } catch {
+            fatalError("Failed to create pipeline state: \(error)")
+        }
+    }
+
+    private static func loadShader(_ name: String) -> MTLFunction {
+        let bundle = Bundle.main
+        guard let path = bundle.path(forResource: name, ofType: "metal") else {
+            fatalError("Failed to load shader")
+        }
+
+        do {
+            let source = try String(contentsOfFile: path, encoding: .utf8)
+            return try MetalManager().device.makeLibrary(source: source, options: nil).device.makeLibrary(source: source, options: nil).makeFunction(name: "main")!
+        } catch {
+            fatalError("Failed to load shader: \(error)")
+        }
+    }
+}
