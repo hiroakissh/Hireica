3af8b1c1ba6f56ad0ad85c6e3ce64b22c28f174a
hiroakissh <hiroaki.saito1014@gmail.com>
2024-01-05 17:52:52 +0900

    [remove] 不要な箇所の削除

--- Hireica/RealTimeRecoderView.swift
+++ Hireica/RealTimeRecoderView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import Combine
 import AVFoundation
 import Charts
-import MetalKit
 
 class TestAudioRecorder: NSObject, ObservableObject  {
     var testAudioRecorder: AVAudioRecorder!
