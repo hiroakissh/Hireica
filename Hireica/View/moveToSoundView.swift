ce4de4b328194bb3f923818c5dd78a3430cabcaf
hiroakissh <hiroaki.saito1014@gmail.com>
2024-03-27 00:21:31 +0900

    [add]ファイルの追加

---
+++ Hireica/View/moveToSoundView.swift
@@ -0,0 +1,18 @@
+//
+//  moveToSoundView.swift
+//  Hireica
+//
+//  Created by HiroakiSaito on 2024/03/27.
+//
+
+import SwiftUI
+
+struct moveToSoundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    moveToSoundView()
+}
