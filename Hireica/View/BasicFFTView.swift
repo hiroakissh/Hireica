8d16811204ffd8ec9f764395c5c7a4af68041af1
hiroakissh <hiroaki.saito1014@gmail.com>
2024-04-04 08:28:31 +0900

    [add] basicViewの追加

---
+++ Hireica/View/BasicFFTView.swift
@@ -0,0 +1,18 @@
+//
+//  BasicFFTView.swift
+//  Hireica
+//
+//  Created by HiroakiSaito on 2024/04/04.
+//
+
+import SwiftUI
+
+struct BasicFFTView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BasicFFTView()
+}
